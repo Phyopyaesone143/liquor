e6460f5b367c90ae46db3cb6a4ee39cb2fe78ca5
Phyo Pyae Sone Win <phyopyaesonemin@Phyos-MacBook-Air.local>
2025-10-01 22:20:53 +0630

    latest

--- liquor/settings.py
+++ liquor/settings.py
@@ -86,6 +86,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#   'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'myprojectdb',
+#     'USER': 'myprojectuser',
+#     'PASSWORD': 'STRONG_PASS',
+#     'HOST': 'localhost',
+#     'PORT': '5432',
+#   }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
